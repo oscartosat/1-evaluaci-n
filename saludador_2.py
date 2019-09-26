f42755798655905b9bd643b65115b0351696fca1
oscartosat <55576392+oscartosat@users.noreply.github.com>
2019-09-26 09:41:57 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,6 @@
+def saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    nombre= raw_input("Como te llamas: ") #Instruccion de entrada
+    print "Bienvenido a python " + nombre
+
+saludador_2()
